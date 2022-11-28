cfa222b3f5f72a1c249f39741a0ee523e44938a3
BSmithberg <innprogressltd@gmail.com>
2022-11-28 10:24:56 -0500

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+//This function is to compute the interest according to the number of years and current rate
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = prinicpal * rate /100;
+
+    //This variable is used to get the correct date and add the amount of years selected to display
+    var year = new Date().getFullYear()+parseInt(years);
+    //This element is used to display text to the user that shows how much interest is earned according to the amounts entered
+    document.getElementById("result").innerHTML="If you deposit"+prinicpal+",\<br\>at an interest rate of"+rate+"%\<br\>You will receive an amount of "+amount+",\<br|>in the year"+year+"\<br\>"
+}
+//This function is to display a number proportional to the slider
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
